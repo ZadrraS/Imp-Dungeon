c75b396c6e9ccfff6214734860a2bc4302ccfc8b
ZadrraS <zadrras@gmail.com>
2010-10-13 21:50:36 +0300

    Added headers that gather all event and message includes.

---
+++ cpp/src/logic/network/events/events.h
@@ -0,0 +1,15 @@
+#ifndef IMPDUNGEON_LOGIC_NETWORK_EVENTS_EVENTS_H_
+#define IMPDUNGEON_LOGIC_NETWORK_EVENTS_EVENTS_H_
+
+#include "logic/network/events/event.h"
+#include "logic/network/events/loginevent.h"
+#include "logic/network/events/logoffevent.h"
+#include "logic/network/events/attackevent.h"
+#include "logic/network/events/takeevent.h"
+#include "logic/network/events/dropevent.h"
+#include "logic/network/events/equipevent.h"
+#include "logic/network/events/useevent.h"
+#include "logic/network/events/moveevent.h"
+#include "logic/network/events/viewupdateevent.h"
+
+#endif  // IMPDUNGEON_LOGIC_NETWORK_EVENTS_EVENTS_H_
